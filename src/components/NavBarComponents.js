8fb6a91583d269f27f46e93cf6b86cbe68dfe94f
atorczynski <a.torczynski@gmail.com>
2020-05-01 01:31:09 +0200

    Adjust navbar. Add link to home.

--- src/components/NavBarComponents.js
+++ src/components/NavBarComponents.js
@@ -3,17 +3,20 @@ import styled from '@emotion/styled';
 export const NavbarElement = styled.header`
   display: flex;
   flex-direction: row;
-  justify-content: center;
+  justify-content: flex-start;
   align-items: center;
   width: 100vw;
   height: 50px;
-  background-color: black;
+  background-color: #111111;
   margin: 0px;
 `;
 
-export const HeadingElement = styled.h1`
+export const HeadingElement = styled.a`
   font-size: 0.95rem;
+  font-weight: bold;
+  text-decoration: none;
   color: white;
+  margin-left: 12rem;
   transition: all 0.5s ease;
 
   &:hover {
