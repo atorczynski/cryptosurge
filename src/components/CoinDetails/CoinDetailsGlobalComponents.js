bad3ca33eaf7fe31e85666519f4c128f2893108e
atorczynski <a.torczynski@gmail.com>
2020-05-06 22:27:20 +0200

    Add global components for page.

---
+++ src/components/CoinDetails/CoinDetailsGlobalComponents.js
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+
+export const CoinDetailsHeading = styled.h2`
+  color: ${(props) => props.theme.text};
+  text-decoration: underline;
+  text-align: center;
+  font-weight: 400;
+`;
