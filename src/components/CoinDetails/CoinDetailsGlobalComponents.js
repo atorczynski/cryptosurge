cbe3ce08fe2d57a44d05d592b6094e460f4beb35
atorczynski <a.torczynski@gmail.com>
2020-05-08 01:29:23 +0200

    Add banner for later usage.

--- src/components/CoinDetails/CoinDetailsGlobalComponents.js
+++ src/components/CoinDetails/CoinDetailsGlobalComponents.js
@@ -6,3 +6,20 @@ export const CoinDetailsHeading = styled.h2`
   text-align: center;
   font-weight: 400;
 `;
+
+export const AddBanner = styled.div`
+  display: flex;
+  height: 250px;
+  width: 100%;
+  border: 1px black solid;
+  border-collapse: collapse;
+  margin-top: 20px;
+`;
+
+export const InfoContainer = styled.div`
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+  background-color: #fff;
+  margin-top: 20px;
+  box-shadow: 0px 9px 15px -5px rgba(0, 0, 0, 0.75);
+`;
