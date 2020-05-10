a6c9aeff5292243498386741ba08c717ac74a27d
atorczynski <a.torczynski@gmail.com>
2020-05-11 01:21:18 +0200

    Add server to resctructure

--- server/index.js
+++ server/index.js
@@ -1,1 +1,17 @@
-import express from 'express';
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log('Server is running on port: ' + port);
+});
