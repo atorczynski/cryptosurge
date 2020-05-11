86c6555e9cef5627ec02787c763a1d88fd958490
atorczynski <a.torczynski@gmail.com>
2020-05-11 02:51:57 +0200

    *

--- server/index.js
+++ server/index.js
@@ -4,10 +4,11 @@ const path = require('path');
 const app = express();
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static('client/build'));
 
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+// index.html for all page routes    html or routing and naviagtion
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
 });
 
 const port = process.env.PORT || 8080;
