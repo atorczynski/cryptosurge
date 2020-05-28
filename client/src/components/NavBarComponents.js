f2432055b1e152efde83a91a60c1ebff18a8af1e
atorczynski <a.torczynski@gmail.com>
2020-05-28 03:58:18 +0200

    Remove old css code from file.

--- client/src/components/NavBarComponents.js
+++ client/src/components/NavBarComponents.js
@@ -1,29 +1,12 @@
 import styled from '@emotion/styled';
 
-export const NavbarElement = styled.header`
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-start;
-  align-items: center;
-  width: 100vw;
-  height: 50px;
-  background-color: #111111;
-  margin: 0px;
-`;
-
 export const HeadingElement = styled.a`
-  font-size: 0.95rem;
-  font-weight: bold;
   text-decoration: none;
-  color: white;
-  margin-left: 10%;
+  color: #fff;
+
   transition: all 0.5s ease;
 
   &:hover {
     color: ${(props) => props.theme.action};
-  }
-
-  @media (max-width: 576px) {
-    margin-left: 50%;
   }
 `;
