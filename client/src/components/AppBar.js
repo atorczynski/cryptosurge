1fb3a4d2b17a364e286673b8c714c895c248d6b3
atorczynski <a.torczynski@gmail.com>
2020-05-29 15:29:51 +0200

    Edit import.

--- client/src/components/AppBar.js
+++ client/src/components/AppBar.js
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import { HeadingElement } from './NavBarComponents';
+import { HeadingElement } from './AppBarComponents';
 
 const useStyles = makeStyles((theme) => ({
   root: {
