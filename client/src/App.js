ddb7a83bdb2ed07d2110720c3bc295f8e1574e2f
atorczynski <artur.torcz@gmail.com>
2020-08-20 01:25:21 +0200

    Add window listener to adjust top padding.

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,36 @@ import { ContentContainer } from './components/BodyHelpers';
 import CookieConsent from 'react-cookie-consent';
 import Footer from './components/Footer/Footer';
 
+function useWindowSize() {
+  const isClient = typeof window === 'object';
+
+  function getSize() {
+    return {
+      width: isClient ? window.innerWidth : undefined,
+      height: isClient ? window.innerHeight : undefined,
+    };
+  }
+
+  const [windowSize, setWindowSize] = React.useState(getSize);
+
+  console.log(windowSize);
+  React.useEffect(() => {
+    if (!isClient) {
+      return false;
+    }
+
+    function handleResize() {
+      setWindowSize(getSize());
+    }
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+  return windowSize;
+}
+
 function App() {
+  const [windowSize] = React.useState(useWindowSize().width);
+  console.log(windowSize);
   return (
     <ThemeProvider theme={primary}>
       <GlobalStyles />
@@ -20,12 +49,15 @@ function App() {
         This website uses cookies to enhance the user experience.
       </CookieConsent>
       <Router>
-        <ContentContainer>
+        <ContentContainer paddingTop={windowSize < '700' ? '50px' : '80px'}>
           <Switch>
             <Route exact path='/'>
               <Main />
             </Route>
             <Route path={'/coins/:id'} component={CoinPage} />
+            <Route path={'/hello'}>
+              <Main />
+            </Route>
           </Switch>
         </ContentContainer>
         <Footer />
