c2aa3c6f7c0fd8ba91d966bf83acda8260cc810f
atorczynski <artur.torcz@gmail.com>
2020-08-20 01:26:14 +0200

    Add Footer component to app.

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {
+  FooterContainer,
+  FooterLink,
+  HeartEmoji,
+  FooterText,
+  FooterContentContainer,
+  FooterLinkExternal,
+} from './FooterComponents';
+
+export default function Footer() {
+  return (
+    <FooterContainer>
+      <FooterContentContainer>
+        <FooterText>Made with </FooterText>
+        <HeartEmoji> ❤️ </HeartEmoji>
+        <FooterText> by </FooterText>
+        <FooterLinkExternal href={'https://github.com/atorczynski'}>
+          Artur Torczynski
+        </FooterLinkExternal>
+      </FooterContentContainer>
+      <FooterContentContainer>
+        <FooterText>Powered by </FooterText>
+        <FooterLinkExternal href={'https://reactjs.org/'}>
+          REACTJS
+        </FooterLinkExternal>
+        <FooterText>|</FooterText>
+        <FooterLinkExternal href={'https://www.coingecko.com/'}>
+          CoinGecko.com
+        </FooterLinkExternal>
+        <FooterText>|</FooterText>
+        <FooterLinkExternal href={'https://coincap.io/'}>
+          CoinCap.io
+        </FooterLinkExternal>
+      </FooterContentContainer>
+      <FooterContentContainer>
+        <FooterLink to={'/disclaimer'}>Privacy & Cookies</FooterLink>
+      </FooterContentContainer>
+    </FooterContainer>
+  );
+}
---
+++ client/src/components/Footer/FooterComponents.js
@@ -0,0 +1,65 @@
+import styled from '@emotion/styled';
+import { Link } from 'react-router-dom';
+
+export const FooterContainer = styled.footer`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  height: 125px;
+  width: 100%;
+  background-color: #333333;
+  margin-top: 50px;
+  color: white;
+  text-align: center;
+`;
+
+export const FooterContentContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+  width: 100%;
+  height: 20px;
+`;
+
+export const HeartEmoji = styled.p`
+  font-size: 25px;
+  margin: 0;
+  color: red;
+  margin-right: 5px;
+`;
+
+export const FooterText = styled.p`
+  margin-right: 5px;
+  font-style: italic;
+  font-weight: 700;
+`;
+export const FooterLinkExternal = styled.a`
+  text-decoration: none;
+  color: red;
+  margin-right: 5px;
+  font-weight: 700;
+  text-transform: uppercase;
+
+  transition: all 0.5s ease;
+  cursor: pointer;
+
+  &:hover {
+    color: #fff;
+  }
+`;
+export const FooterLink = styled(Link)`
+  text-decoration: none;
+  color: red;
+  margin-right: 5px;
+  font-weight: 700;
+  text-transform: uppercase;
+
+  transition: all 0.5s ease;
+  cursor: pointer;
+
+  &:hover {
+    color: #fff;
+  }
+`;
