254acf513d26c7c452c13e52454a0cc478a1ced6
atorczynski <artur.torcz@gmail.com>
2020-08-20 01:25:37 +0200

    Change styling of app.

--- client/src/components/AppBarComponents.js
+++ client/src/components/AppBarComponents.js
@@ -2,11 +2,11 @@ import styled from '@emotion/styled';
 
 export const HeadingElement = styled.a`
   text-decoration: none;
-  color: #fff;
+  color: red;
 
   transition: all 0.5s ease;
 
   &:hover {
-    color: ${(props) => props.theme.action};
+    color: #fff;
   }
 `;
