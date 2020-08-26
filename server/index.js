a2cc9d7d9fee606b7a35dd6fc13259468f1596f2
atorczynski <artur.torcz@gmail.com>
2020-08-26 21:31:52 +0200

    Fix cors issue.

--- server/index.js
+++ server/index.js
@@ -10,6 +10,15 @@ const routes = require('./newsApi');
 
 const app = express();
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100,
