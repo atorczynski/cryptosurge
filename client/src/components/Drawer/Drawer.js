5c08c4393905e4f9891805942ae5a251563ffafe
atorczynski <artur.torcz@gmail.com>
2020-09-11 03:01:14 +0200

    Adjust Drawer component to be responsive.

--- client/src/components/Drawer/Drawer.js
+++ client/src/components/Drawer/Drawer.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import { Button, Collapse } from '@material-ui/core';
+import CloseIcon from '@material-ui/icons/Close';
+
+const CloaseIconStyled = styled(CloseIcon)`
+  cursor: pointer;
+`;
 
 const buttonStyle = {
   size: '20px',
@@ -9,15 +14,32 @@ const buttonStyle = {
   textTransform: 'none',
   display: 'inherit',
   marginBottom: '10px',
+  marginTop: '20px',
 };
 
 const DrawerConatiner = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+  align-items: center;
   background-color: inherit;
-  width: inherit;
-  height: 100px;
+  width: auto;
+  height: auto;
+  margin-bottom: 10px;
+
+  @media (max-width: 379px) {
+    margin-left: 10px;
+  }
 `;
 
-export default function Drawer({ buttonHeading, drawerContent }) {
+const DrawerHeading = styled.h3``;
+
+export default function Drawer({
+  buttonHeading,
+  drawerContent,
+  drawerHeading,
+}) {
   const [isOpen, setOpen] = React.useState(false);
 
   const handleClick = () => {
@@ -36,7 +58,15 @@ export default function Drawer({ buttonHeading, drawerContent }) {
         Read more about {buttonHeading}
       </Button>
       <Collapse in={isOpen}>
-        <DrawerConatiner>{drawerContent}</DrawerConatiner>
+        <DrawerConatiner>
+          <DrawerHeading>{drawerHeading}</DrawerHeading>
+          {drawerContent}
+          <CloaseIconStyled
+            onClick={() => {
+              handleClick();
+            }}
+          />
+        </DrawerConatiner>
       </Collapse>
     </React.Fragment>
   );
