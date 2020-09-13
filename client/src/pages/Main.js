2fe77c37dee5935a084f2d3c2224271bd0015c3b
atorczynski <artur.torcz@gmail.com>
2020-09-13 16:34:39 +0200

    Prepare trending coins container.

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -17,6 +17,17 @@ const Container = styled.div`
     align-items: center;
   }
 `;
+const CoinContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+
+  @media (max-width: 379px) {
+    flex-direction: column;
+    align-items: center;
+  }
+`;
 
 const Jumbotron = styled.div`
   display: flex;
@@ -43,8 +54,17 @@ const JumbotronCaption = styled.h4`
   top: 400px;
 `;
 
+const TrendingCoinsContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-content: flex-start;
+  width: 25%;
+  flex-wrap: wrap;
+`;
+
 export default function Main() {
   const [coins, setCoins] = React.useState([]);
+  const [trendingCoins, setTrendingCoins] = React.useState([]);
   const [isLoading, setLoading] = React.useState();
 
   const createTrendColor = (currentChange) => {
@@ -61,6 +81,13 @@ export default function Main() {
       const response = await fetch(`https://api.coingecko.com/api/v3/coins/`);
       const data = await response.json();
       setCoins(data);
+
+      const trendingResponse = await fetch(
+        'https://api.coingecko.com/api/v3/search/trending'
+      );
+      const trendingData = await trendingResponse.json();
+      setTrendingCoins(trendingData.coins);
+      console.log(trendingData.coins);
     } catch (error) {
       console.error(error);
     } finally {
@@ -106,34 +133,48 @@ export default function Main() {
       {isLoading ? (
         <LoadingCoinRender />
       ) : (
-        coins.map((coin) => (
-          <Coin
-            trendColor24h={createTrendColor(
-              coin.market_data.price_change_percentage_24h
-            )}
-            trendColor7d={createTrendColor(
-              coin.market_data.price_change_percentage_7d
-            )}
-            trendColor30d={createTrendColor(
-              coin.market_data.price_change_percentage_30d
-            )}
-            link={coin.id}
-            key={coin.id}
-            coinName={coin.id}
-            coinLogo={coin.image.thumb}
-            change24h={cutFloatValue(
-              coin.market_data.price_change_percentage_24h
-            )}
-            change7d={cutFloatValue(
-              coin.market_data.price_change_percentage_7d
-            )}
-            change30d={cutFloatValue(
-              coin.market_data.price_change_percentage_30d
-            )}
-            currentPrice={coin.market_data.current_price.usd}
-          />
-        ))
+        <CoinContainer>
+          {coins.map((coin) => (
+            <Coin
+              trendColor24h={createTrendColor(
+                coin.market_data.price_change_percentage_24h
+              )}
+              trendColor7d={createTrendColor(
+                coin.market_data.price_change_percentage_7d
+              )}
+              trendColor30d={createTrendColor(
+                coin.market_data.price_change_percentage_30d
+              )}
+              link={coin.id}
+              key={coin.id}
+              coinName={coin.id}
+              coinLogo={coin.image.thumb}
+              change24h={cutFloatValue(
+                coin.market_data.price_change_percentage_24h
+              )}
+              change7d={cutFloatValue(
+                coin.market_data.price_change_percentage_7d
+              )}
+              change30d={cutFloatValue(
+                coin.market_data.price_change_percentage_30d
+              )}
+              currentPrice={coin.market_data.current_price.usd}
+            />
+          ))}
+        </CoinContainer>
       )}
+      {/* {isLoading ? (
+        ''
+      ) : (
+        <TrendingCoinsContainer>
+          {trendingCoins.map((trendingCoin) => (
+            <Coin
+              coinName={trendingCoin.item.name}
+              coinLogo={trendingCoin.item.thumb}
+            />
+          ))}
+        </TrendingCoinsContainer>
+      )} */}
     </Container>
   );
 }
